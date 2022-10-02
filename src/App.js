12b936f9dca9f6e421f161cb9f14f5271daa7931
RafaelB <rafaelboschmamail@gmail.com>
2022-10-02 21:12:45 +0200

    Finished start page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
 
-function App() {
+const App = () => {
+  const [level, setLevel] = useState(1);
+  const [score, setScore] = useState(0);
+  const [clicked, setClicked] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <div id="nav">
+        <h1>Formula 1 Memory Card</h1>
+      </div>
+      <div id="content">
+        <h3>Welcome to F1 memory card!<br></br>Click all images without clicking the same image twice.</h3>
+        <button id="start">START</button>
+      </div>
+      <div id="footer">
+        <a href="">GITHUB</a>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
